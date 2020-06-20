5591474aa1a7661fe2ea31fe4b0c47742f0674f2
Spencer <26808399+scwhaley@users.noreply.github.com>
2020-06-20 10:57:59 -0700

    Initial commit of WPF application

---
+++ Ladder-GUI/Ladder-GUI-WPF/Models/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Printing;
+
+namespace Ladder_GUI_WPF.Model
+{
+    class User : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int _userId;
+        private string firstName;
+        private string lastName;
+
+        public int UserID
+        {
+            get
+            {
+                return _userId;
+            }
+
+            set
+            {
+                _userId = value;
+            }
+        }
+
+    }
+}
