76a56c486384bf26b2474019ab02cdc159b09c7d
Spencer <26808399+scwhaley@users.noreply.github.com>
2020-06-23 21:51:14 -0700

    Linked RungViewModel to RungModel

--- Ladder-GUI/Ladder-GUI-WPF/ViewModels/Rung ViewModels/RungViewModel.cs
+++ Ladder-GUI/Ladder-GUI-WPF/ViewModels/Rung ViewModels/RungViewModel.cs
@@ -7,6 +7,18 @@ namespace Ladder_GUI_WPF
     class RungViewModel : BaseViewModel
     {
         #region Properties
+        private IRungModel _rungModel;
+        public IRungModel RungModel { get => _rungModel;
+            set
+            {
+                if (_rungModel == value)
+                    return;
+
+                _rungModel = value;
+                OnPropertyChanged();
+            }
+        }
+
         private int _rungNunmber;
         public int RungNumber { get => _rungNunmber;
             set
@@ -18,13 +30,24 @@ namespace Ladder_GUI_WPF
                 OnPropertyChanged();
             }
         }
-        private ObservableCollection<IBaseInstructionModel> RungInstructions { get; set; } = new ObservableCollection<IBaseInstructionModel>();
+        public ObservableCollection<BaseInstructionViewModel> RungInstructions { get; set; } = new ObservableCollection<BaseInstructionViewModel>();
         #endregion Properties
 
+        #region Constructor
+        public RungViewModel()
+        {
+            IRungModel RungModel = new RungModel();
+        }
+        #endregion Constructor
+
+        #region Methods
+        // Adds an instruction to the rung models linked list of instructions
         public void addInstruction(int index, IBaseInstructionModel instruction)
         {
-            RungInstructions.Insert(index, instruction);
+            RungModel.LinkedListOfInstructions.AppendInstruction(instruction, index);
         }
+
+        #endregion Methods
 
     }
 }
