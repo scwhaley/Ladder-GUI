cf7519ee1c0fd806ef72e7978367837232184d98
Spencer <26808399+scwhaley@users.noreply.github.com>
2020-06-29 21:40:17 -0700

    Finished updating algorithm, but now VMID is 0 for all but the first created instruction

--- Ladder-GUI/Ladder-GUI-WPF/ViewModels/Rung ViewModels/RungViewModel.cs
+++ Ladder-GUI/Ladder-GUI-WPF/ViewModels/Rung ViewModels/RungViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Ladder_GUI_WPF.Models;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
@@ -58,7 +59,8 @@ namespace Ladder_GUI_WPF
         public RungViewModel()
         {
             RungModel = new RungModel();
-            GridFormatInstructionList = new ObservableCollection<BaseInstructionViewModel>() { new BaseInstructionViewModel() { } }
+            GridFormatInstructionList = new ObservableCollection<BaseInstructionViewModel>();
+
             File.AppendAllText(@"C:\Temp\Debug.txt", $"{DateTime.Now.ToString("HH:mm:ss")} - Created new RungViewModel {Environment.NewLine}");
         }
         #endregion Constructor
@@ -75,39 +77,55 @@ namespace Ladder_GUI_WPF
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
             InstructionViewModelFactory factory = new InstructionViewModelFactory();
+            BaseInstructionViewModel instructionViewModel;
 
             RungInstructionsLinkedListNode node = RungModel.LinkedListOfInstructions.Head;
             int rowIndex = 0;
             int columnIndex = 0;
             int loopNum = 0;
-
+            bool difference = false;
             File.AppendAllText(@"C:\Temp\Debug.txt", $"Setup ElapsedTime: {stopwatch.ElapsedMilliseconds}ms {Environment.NewLine}");
 
             Stopwatch loopWatch = Stopwatch.StartNew();
+            // Walk the InstructionList and verify that it matches the GridFormatInstructionList.
+            // If there is a node that doesn't match, start recreating the GridFormatList from there.
+            // Better than fully recreating each time, but only marginal benefit for changes near the beginning of the list.
             while (node != null)
             {
                 loopWatch.Restart();
-                //               File.AppendAllText(@"C:\Temp\Debug.txt", $"{DateTime.Now.ToString("HH:mm:ss")} - Try to create InstructionViewModel for instruction node {node.ID}  {Environment.NewLine}");
-                if (node.ID != GridFormatInstructionList[loopNum].VMID)
+
+                switch (GridFormatInstructionList.Count <= loopNum)
                 {
-                    File.AppendAllText(@"C:\Temp\Debug.txt", $"BEFORE FACTORY: {stopwatch.ElapsedMilliseconds}ms {Environment.NewLine}");
-                    BaseInstructionViewModel instructionViewModel = factory.CreateInstructionViewModel(node.Instruction);
-                    File.AppendAllText(@"C:\Temp\Debug.txt", $"AFTER  FACTORY: {stopwatch.ElapsedMilliseconds}ms {Environment.NewLine}");
+                    // If we need to extend the list
+                    case true:
+                        instructionViewModel = factory.CreateInstructionViewModel(node.Instruction);
+                        GridFormatInstructionList.Add(instructionViewModel);
+                        instructionViewModel.RowIndex = rowIndex;
+                        instructionViewModel.ColumnIndex = columnIndex;
+                        instructionViewModel.VMID = node.ID;
+                        File.AppendAllText(@"C:\Temp\Debug.txt", $"VM {columnIndex}.{instructionViewModel.VMID} created and added to list ElapsedTime: {stopwatch.ElapsedMilliseconds}ms {Environment.NewLine}");
+                        break;
 
-                    instructionViewModel.RowIndex = rowIndex;
-                    instructionViewModel.ColumnIndex = columnIndex;
-                    instructionViewModel.VMID = node.ID;
-
-                    GridFormatInstructionList[loopNum] = instructionViewModel;
-                    //File.AppendAllText(@"C:\Temp\Debug.txt", $"{DateTime.Now.ToString("HH:mm:ss")} -Added InstructionvViewModel to GridFormatInstructionList with RowIndex {rowIndex} and ColumnIndex {columnIndex} {Environment.NewLine}");
+                    // If we only need to replace elements. Difference represents that an element has changed, so we can't rely on the existing GridFormatList any more.
+                    case false:
+                        if (node.ID != GridFormatInstructionList[loopNum].VMID || difference)
+                        {
+                            instructionViewModel = factory.CreateInstructionViewModel(node.Instruction);
+                            GridFormatInstructionList[loopNum] = instructionViewModel;
+                            instructionViewModel.RowIndex = rowIndex;
+                            instructionViewModel.ColumnIndex = columnIndex;
+                            instructionViewModel.VMID = node.ID;
+                            difference = true;
+                            File.AppendAllText(@"C:\Temp\Debug.txt", $"VM {columnIndex}.{instructionViewModel.VMID} created and replaced in list ElapsedTime: {stopwatch.ElapsedMilliseconds}ms {Environment.NewLine}");
+                        }
+                        break;
                 }
-                File.AppendAllText(@"C:\Temp\Debug.txt", $"BEFORE FACTORY: {stopwatch.ElapsedMilliseconds}ms {Environment.NewLine}");
-                
+
                 columnIndex++;
                 loopNum++;
                 node = node.Next;
+                File.AppendAllText(@"C:\Temp\Debug.txt", $"Finished Checking VM{columnIndex - 1} ElapsedTime: {stopwatch.ElapsedMilliseconds}ms {Environment.NewLine}");
 
-                File.AppendAllText(@"C:\Temp\Debug.txt", $"VM {columnIndex-1} created and added to list ElapsedTime: {stopwatch.ElapsedMilliseconds}ms {Environment.NewLine}");
             }
 
             File.AppendAllText(@"C:\Temp\Debug.txt", $"CreateGridFormatRung ElapsedTime: {stopwatch.ElapsedMilliseconds}ms {Environment.NewLine}");
