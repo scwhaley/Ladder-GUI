d230deee0384730d631640fc30ff199d4605179f
Spencer <26808399+scwhaley@users.noreply.github.com>
2020-07-16 16:39:10 -0700

    Updated LadderRoutineViewModel with AddRung and SelectedRungChanged commands

--- Ladder-GUI/Ladder-GUI-WPF/ViewModels/Routine ViewModels/LadderRoutineViewModel.cs
+++ Ladder-GUI/Ladder-GUI-WPF/ViewModels/Routine ViewModels/LadderRoutineViewModel.cs
@@ -1,41 +1,68 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows.Documents;
+using System.Windows.Input;
 
 namespace Ladder_GUI_WPF
 {
     class LadderRoutineViewModel : BaseRoutineViewModel
     {
-        private ObservableCollection<RungViewModel> _rungList;
-        public ObservableCollection<RungViewModel> RungList
+        public ICommand AddRungCommand { get; set; }
+        public ICommand AddRungFromModelCommand { get; set; }
+        public ICommand SelectedRungChangedCommand { get; set; }
+
+        private ObservableCollection<RungViewModel> _rungVMList;
+        public ObservableCollection<RungViewModel> RungVMList
         {
-            get { return _rungList; }
+            get { return _rungVMList; }
             set 
             {
-                if (_rungList == value)
+                if (_rungVMList == value)
                     return;
 
-                _rungList = value;
-                OnPropertyChanged(nameof(RungList));
+                _rungVMList = value;
+                OnPropertyChanged(nameof(RungVMList));
             }
         }
+
+        private RungViewModel _selectedRung;
+
+        public RungViewModel SelectedRung
+        {
+            get { return _selectedRung; }
+            set 
+            { 
+                if( _selectedRung == value)
+                    return;
+
+                _selectedRung = value;
+                OnPropertyChanged(nameof(SelectedRung));
+            }
+        }
+
 
         #region Constructor
         public LadderRoutineViewModel(LadderRoutineModel routineModel) : base(routineModel)
         {
-            routineModel.RungList
+            RungVMList = new ObservableCollection<RungViewModel>();
+            AddRungCommand = new RelayCommand<object>(AddRung);
+            AddRungFromModelCommand = new RelayCommand<RungModel>(AddRungFromModel);
+            SelectedRungChangedCommand = new RelayCommand<object>(SelectedRungChanged);
         }
         #endregion Constructor
 
-        private ObservableCollection<RungViewModel> GenerateRungViewModelList(List<RungModel> modelList)
+        private void AddRung(object obj)
         {
-            ObservableCollection<RungViewModel> viewModelObservableCollection = new ObservableCollection<RungViewModel>();
-            foreach (RungModel model in modelList)
-            {
-                viewModelObservableCollection.Add(new RungViewModel(model));
-            }
-
-            return viewModelObservableCollection;
+            RungVMList.Add(new RungViewModel(new RungModel()));
+        }
+        private void AddRungFromModel(RungModel rungModel)
+        {
+            RungVMList.Add(new RungViewModel(rungModel));
+        }
+        public void SelectedRungChanged(object obj)
+        {
+            var rung = obj as RungViewModel;
+            SelectedRung = rung;
         }
     }
 }
